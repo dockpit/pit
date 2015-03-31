c5ce4313fea65b3dcf011896247a7d337bd4ffa5
Ad van der Veer <advanderveer@gmail.com>
2015-03-31 11:34:43 +0200

    added some minimal tracking and incremented version

--- server/ui/src/js/app.js
+++ server/ui/src/js/app.js
@@ -1,9 +1,13 @@
 var React = require('react');
-var Dispatcher = require('flux').Dispatcher;
+var Dispatcher = require('./dispatcher/AppDispatcher')
 
 var IsolationPanel = require("./components/IsolationPanel.jsx");
 var DepPanel = require("./components/DepPanel.jsx");
 var EditorWorkspace = require("./components/EditorWorkspace.jsx");
+
+var DepActions = require('./actions/DepActions')
+var IsolationActions = require('./actions/IsolationActions')
+var EditorActions = require('./actions/EditorActions')
 
 //render panels on pages that contain them
 //replace this later for a true one-page-app
@@ -24,3 +28,28 @@ if (ew) {
   var parts = window.location.pathname.split('/')
   React.render(<EditorWorkspace depName={parts[2]} stateName={parts[4]}/>, ew)
 }
+
+// track certain action
+Dispatcher.register(function(a){
+  switch (a.type) {
+    case DepActions.CREATE:
+      analytics.track('Created Dep');
+      break
+
+    case DepActions.ADD_DEP_STATE:
+      analytics.track('Created Dep State');
+      break
+
+    case IsolationActions.CREATE:
+      analytics.track('Created Isolation');
+      break
+
+    case IsolationActions.ADD_STATE:
+      analytics.track('Added Dep State to Isolation');
+      break
+
+    case EditorActions.START_BUILD:
+      analytics.track('Started Build');
+      break
+  }
+})
